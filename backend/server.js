f0584d91917cfedf1c15cd5272ad31667ab7f8b7
lokurawat <rawatlokesh960@gmail.com>
2026-04-08 12:26:08 +0530

    intial commit

---
+++ backend/server.js
@@ -0,0 +1,40 @@
+// server.js
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+
+import { connectDB } from './config/db.js';  // ← use named import here
+import authRoutes from './routes/authRoutes.js';
+import resumeRoutes from './routes/resumeRoutes.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const PORT = 4000;
+
+// Middleware to handle CORS
+app.use(cors());
+
+// Connect Database
+connectDB();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/resume', resumeRoutes);
+
+// Server uploads folder
+app.use(
+  '/uploads',
+  express.static(path.join(__dirname, 'uploads'), {
+    setHeaders: (res, _path) => {
+      res.set('Access-Control-Allow-Origin', 'https://resumexpert-frontend.onrender.com');
+    },
+  })
+);
